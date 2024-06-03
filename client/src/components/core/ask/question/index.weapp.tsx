27d40b18f6705f6a632eb2626f2af4dd91703b84
ekansrm <ekansrm0001@163.com>
2024-06-03 18:29:54 +0800

    添加组件

--- client/src/components/core/ask/question/index.weapp.tsx
+++ client/src/components/core/ask/question/index.weapp.tsx
@@ -1,9 +1,19 @@
 import { View } from '@tarojs/components';
 
 import { Component } from 'react'
+import QuestionComponent from "@/components/core/ask/question/question";
 
 export default class Quest extends Component {
   render() {
-    return <View>问答组件</View>;
+    return <View>
+      <QuestionComponent
+        question={{
+          title: '以下哪个不是 JavaScript 的数据类型？',
+          options: ['Number', 'String', 'Boolean', 'Object'],
+          type: 'single'
+        }}
+      />
+
+    </View>;
   }
 }
