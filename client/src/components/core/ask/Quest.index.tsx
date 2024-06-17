df59842c26d797b2994bf2735f6b7c5bfec1417b
ekansrm <ekansrm0001@163.com>
2024-06-17 20:00:16 +0800

    状态视觉分离

--- client/src/components/core/ask/Quest.index.tsx
+++ client/src/components/core/ask/Quest.index.tsx
@@ -1,15 +1,24 @@
-import {View} from "@tarojs/components";
 import Question from '@/components/core/ask/Question.index';
+import {QuestionData} from "@/components/core/ask/model/types";
+import QuestView from "@/components/core/ask/Quest.view";
 
-const Quest = ({ questions }) => {
+interface Props {
+  questions: QuestionData[];
+}
+
+
+const Quest = ({ questions }: Props) => {
   return (
-    <View>
-      {questions.map((question) => (
-        <View key={question.qid}>
-            <Question question={question} answered={false} />
-        </View>
-      ))}
-    </View>
+    <QuestView
+      questionDataList={questions}
+      questionPackBuilder={
+        (question) => {
+          return {
+            questionView: <Question question={question}  answered={false} />,
+          }
+        }
+      }
+    />
   );
 };
 
---
+++ client/src/components/core/ask/Quest.view.tsx
@@ -0,0 +1,29 @@
+import {View} from "@tarojs/components";
+
+import {QuestionData} from "@/components/core/ask/model/types";
+import React from "react";
+
+interface QuestionPack {
+
+  questionView: React.ReactNode;
+
+}
+
+interface Props {
+
+  questionDataList: QuestionData[];
+  questionPackBuilder: (question: QuestionData) => QuestionPack;
+}
+
+
+const QuestView = ({ questionDataList, questionPackBuilder}: Props) => {
+  return (
+    <View>
+      {questionDataList.map((question) => (
+        questionPackBuilder(question).questionView
+      ))}
+    </View>
+  );
+};
+
+export default QuestView;
