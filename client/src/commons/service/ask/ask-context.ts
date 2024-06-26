c7e87e510fee239b24f7aa2807282951c625dc21
ekansrm <ekansrm0001@163.com>
2024-06-26 21:44:31 +0800

    整理结构

--- client/src/commons/service/ask/ask-context.ts
+++ client/src/commons/service/ask/ask-context.ts
@@ -1,24 +1,45 @@
 import {Question} from "@/commons/model/ask/question";
 import {PropertyValue} from "@/commons/model/ask/property";
 
-export class AskContext {
+// export class AskContext {
+//
+//   // 问题集合, {qid: QuestionIndex}
+//   questions: { [qid: string]: Question } = {}
+//
+//   // 属性集合, {pid: PropertyValue}
+//   propertyPool: { [pid: string]: PropertyValue } = {}
+//
+//   // 属性集合历史
+//   propertyPoolHistory: { [qNum: string]: { [pid: string]: PropertyValue } } = {}
+//
+//
+//   questionAnswered: {[qid:string]: boolean} = {};
+//
+//   // 回答次序， [qid]
+//   questionAnsweredOrder: { [qid: string]: number } = {}
+//
+//   // 回答选项， {qid: [oid]}
+//   questionOptionChosen: { [qid: string]: {[oid:string]:boolean} } = {}
+//
+// }
+export interface AskContext {
 
   // 问题集合, {qid: QuestionIndex}
-  questions: { [qid: string]: Question } = {}
+  questions: { [qid: string]: Question }
+
+  questionAnswered: {[qid:string]: boolean}
+
+  questionOptionChosen: { [qid: string]: {[oid:string]:boolean} }
 
   // 属性集合, {pid: PropertyValue}
-  propertyPool: { [pid: string]: PropertyValue } = {}
-
-  // 属性集合历史
-  propertyPoolHistory: { [qNum: string]: { [pid: string]: PropertyValue } } = {}
-
-
-  questionAnswered: {[qid:string]: boolean} = {};
+  propertyPool: { [pid: string]: PropertyValue }
 
   // 回答次序， [qid]
-  questionAnsweredOrder: { [qid: string]: number } = {}
+  questionAnsweredOrder: { [qid: string]: number }
+
+  // 属性集合历史
+  propertyPoolHistory: { [qNum: string]: { [pid: string]: PropertyValue } }
 
   // 回答选项， {qid: [oid]}
-  questionOptionChosen: { [qid: string]: {[oid:string]:boolean} } = {}
 
 }
--- client/src/components/core/ask/Quest.state.ts
+++ client/src/components/core/ask/Quest.state.ts
@@ -1,10 +1,44 @@
 import {makeAutoObservable} from "mobx";
+import {AskContext} from "@/commons/service/ask/ask-context";
+import {Question} from "@/commons/model/ask/question";
+import {route} from "@/commons/service/ask/ask-engine";
 
 
 class QuestState {
 
   constructor() {
     makeAutoObservable(this);
+  }
+
+  context: AskContext = {
+    questions: {},
+    propertyPool: {},
+    propertyPoolHistory: {},
+    questionAnsweredOrder: {},
+    questionOptionChosen: {},
+    questionAnswered: {},
+  };
+
+  loadQuestions(questions: Question[]) {
+    questions.forEach(question => {
+      this.context.questions[question.qid] = question;
+      this.questionAnswered[question.qid] = false;
+      this.questionOptionChosen[question.qid] = {};
+    });
+  }
+
+  get questionOptionChosenN() {return this.context.questionOptionChosen}
+
+  get questCurrent() {
+    // 按顺序列出已回答的问题
+    const questions: Question[] = []
+
+    const pendingQuestionId = route(this.context);
+    if(pendingQuestionId !== '') {
+      questions.push(this.context.questions[pendingQuestionId]);
+    }
+    return questions;
+
   }
 
   questionAnswered: {[qid:string]: boolean} = {};
