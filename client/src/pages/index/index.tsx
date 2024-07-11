67861fb10612603757b1a2343069b1c533bb64da
KAMI-Wei <ekansrm0001@163.com>
2024-07-12 00:18:02 +0800

    解决文件加载问题

--- client/src/pages/index/index.tsx
+++ client/src/pages/index/index.tsx
@@ -6,6 +6,7 @@ import { View } from '@tarojs/components'
 import Ask from "@/components/core/ask/index.weapp";
 // import TestMobx from "@/components/core/ask/TestMobx";
 import './index.scss'
+import path from "path";
 
 
 
@@ -32,6 +33,63 @@ export default class Index extends Component<PropsWithChildren> {
         console.log(err)
       }
     })
+
+
+    const fs = Taro.getFileSystemManager();
+    fs.readdir({
+      dirPath: `${Taro.env.USER_DATA_PATH}`,
+      success(res) {
+        console.log(res)
+      },
+      fail(res) {
+        console.log(res)
+      }
+
+    })
+
+    fs.getFileInfo({
+      filePath: `${Taro.env.USER_DATA_PATH}`,
+      success(res) {
+        console.log(res)
+      },
+      fail(res) {
+        console.log(res)
+      }
+
+    })
+
+    fs.mkdir({
+      dirPath: '123',
+      success(res) {
+        console.log(res)
+      },
+      fail(res) {
+        console.log(res)
+      }
+    })
+
+    fs.readFile(
+      {
+        filePath: `${Taro.env.USER_DATA_PATH}/ask.json`,
+        encoding: 'utf8',
+        success(res) {
+          if( typeof res.data === 'string' ) {
+            const json = JSON.parse(res.data)
+            console.log(json)
+          } else {
+            console.log(res.data)
+          }
+        },
+        fail(err) {
+          console.log(err)
+        }
+      }
+    )
+    // const res = fs.readFileSync(
+    //   'comp.json', 'utf8', 0,
+    // )
+    //
+    // console.log(res)
 
 
     return (
